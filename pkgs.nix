e66b2284eed35a519346752a94ac930edffbe9f8
Djangodv <dd.manders@protonmail.com>
2026-06-23 19:54:48 +0200

    feat: transferred existing home-manager configuration

---
+++ pkgs.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+
+    # Allow the installation of unfree software
+    nixpkgs.config.allowUnfree = true;
+
+    home.packages = with pkgs; [
+        spotify
+        gimp
+        tldr
+        ripgrep
+        fzf
+        anki
+        x11docker
+        zip
+        # lutris
+        libreoffice-qt # Qt framework is better suited for KDE Plasma
+        pandoc
+        texliveBasic # See: https://wiki.nixos.org/wiki/TexLive
+        kdePackages.kdenlive # Video editor
+        kdePackages.filelight
+        # mc
+    ];
+}

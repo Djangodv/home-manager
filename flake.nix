2b317900d438b73a9d05ae432e9af4ed7eb102b9
Djangodv <dd.manders@protonmail.com>
2026-06-25 11:41:39 +0200

    build: specified exact dependency versions in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,33 @@
 {
-  description = "Nix flake used by home-manager";
+  description = "Home Manager configuration of user";
 
   inputs = {
-    # NixOS official package source, using the nixos-26.05 branch
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    # home-manager, used for managing user configuration
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    # Home-manager, used for managing user configuration
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+
     neovim-nightly-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, plasma-manager, ... }:
+  outputs =
+    inputs@{ nixpkgs, home-manager, plasma-manager, ... }:
     let
       system = "x86_64-linux";
-      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
 
       overlays = [
         inputs.neovim-nightly-overlay.overlays.default
